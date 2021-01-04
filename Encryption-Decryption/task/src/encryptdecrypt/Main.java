3477c2d8b8ae84dca1411ac0eb38575983063e16
feztix <pan0v.s.s@yandex.ru>
2021-01-05 01:36:10 +0300

    Stage 1/6 Initial Commit

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -2,6 +2,7 @@ package encryptdecrypt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //we found a treasure! >> dv ulfmw z givzhfiv!
+        System.out.println("dv ulfmw z givzhfiv!");
     }
 }
